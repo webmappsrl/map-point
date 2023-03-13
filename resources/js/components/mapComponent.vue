02d9a90a3c9bd4e560cf9a9a71b49563e0199702
RubensGarofalo <rubensgiuseppegarofalo@gmail.com>
2023-03-13 15:03:43 +0100

    ...

--- resources/js/components/mapComponent.vue
+++ resources/js/components/mapComponent.vue
@@ -16,7 +16,7 @@
         <label class="flex-street-label inline-block pt-2 leading-tight">
             Street Name
         </label>
-        <input type="text" :disabled="!edit" :class="{ 'form-input-black': edit }" class="flex-street-input form-control form-input form-input-bordered shadow-lg">
+        <input @input="updateStreetAddress($event)"  type="text" :disabled="!edit" :class="{ 'form-input-black': edit }" class="flex-street-input form-control form-input form-input-bordered shadow-lg">
     </div>
   </div>
   <div id="container">
@@ -78,6 +78,7 @@
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import axios from 'axios';
 const DEFAULT_TILES = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const DEFAULT_ATTRIBUTION = '<a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>';
 const DEFAULT_CENTER = [42, 12];
@@ -100,7 +101,9 @@ export default {
                 fillOpacity: 1,
                 radius: 100
             },
-            mapDiv:null
+            mapDiv:null,
+            streetAddress:null,
+            api:'https://nominatim.openstreetmap.org/ui/search.html?q=',
         }
     },
     methods: {
@@ -165,7 +168,28 @@ export default {
             this.lat = lat;
             this.lng = lng;
             this.map.panTo(new L.LatLng(lat,lng));
-        }
+        },
+        async updateStreetAddress(event) {
+            clearTimeout(this.debounce);
+            this.debounce = setTimeout(async () => {
+                console.log(event.target.value);
+                //var res  = await axios.get(this.api+event.target.value,opt);
+                var res = [
+                    {"place_id":113213715,"licence":"Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright","osm_type":"way","osm_id":24994966,"boundingbox":["37.5128627","37.5132466","15.0861492","15.0880696"],"lat":"37.5129407","lon":"15.0874726","display_name":"Via Redentore, Monserrato, Borgo-Sanzio, Catania, Sicilia, 95027, Italia","place_rank":26,"category":"highway","type":"residential","importance":0.30000999999999994,"geojson":{"type":"LineString","coordinates":[[15.0861492,37.5132466],[15.0864485,37.513127],[15.0874726,37.5129407],[15.087767,37.5128681],[15.0880696,37.5128627]]}}
+                ];
+                try{
+                    const lat = res[0].lat;
+                    const lng = res[0].lon;
+                    this.updateLatLng(lat,lng);
+                }catch(error){
+                    console.log(error);
+                }
+            }, 600)
+        },
+        async reverseGeoCoding(lat, lng){
+            var api = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=18&addressdetails=1`;
+            var res  = await axios.get(api);
+        }    
     },
     mounted() {
         this.initMap();
